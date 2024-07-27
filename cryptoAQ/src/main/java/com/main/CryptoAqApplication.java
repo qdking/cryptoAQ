474bc64a74e0a7a7ffb9f388fae0474fe791b0a8
QD <61643303+qdking@users.noreply.github.com>
2024-07-27 17:50:47 +0800

    Add files via upload
    Initial commit

---
+++ cryptoAQ/src/main/java/com/main/CryptoAqApplication.java
@@ -0,0 +1,14 @@
+package com.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class CryptoAqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoAqApplication.class, args);
+	}
+
+}
